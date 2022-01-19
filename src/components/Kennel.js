3739e1e640152a1d682379a09bc3982e0d78d8e9
Sullivan Pierce <sullypierce@gmail.com>
2022-01-19 13:59:10 -0500

    initial setup and push to repo

---
+++ src/components/Kennel.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { LocationList } from "./location/LocationList.js"
+import "./Kennel.css"
+
+export const Kennel = () => (
+    <>
+        <h2>Nashville Kennels</h2>
+        <LocationList />
+    </>
+)
---
+++ src/components/location/LocationList.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+export const LocationList = () => (
+    <article className="locations">
+        <h2>List of Locations</h2>
+    </article>
+)
